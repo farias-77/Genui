a0a04bf26dc9edd0b3a4701edac5a0b45498cd8b
Gabriel Farias <gabrielfalves2003@gmail.com>
2024-09-01 14:00:34 -0300

    saving progress

--- data/main.py
+++ data/main.py
@@ -1,9 +1,18 @@
 # main.py
 import os
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 from orquestradorController import router as orquestrador_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permite apenas o frontend React
+    allow_credentials=True,
+    allow_methods=["*"],  # Permite todos os métodos (GET, POST, etc.)
+    allow_headers=["*"],  # Permite todos os cabeçalhos
+)
 
 # Registrando o router da OrquestradorController
 app.include_router(orquestrador_router, prefix="/orquestrador")
